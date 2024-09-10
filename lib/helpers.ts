6acf95fc57ba21a2ec065013f619e40db5d7c06c
vampo <umangboss786@gmail.com>
2024-09-10 20:08:23 +0530

    Fix NFT minting and updating functions

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -2,13 +2,15 @@ import {
   create,
   fetchCollection,
   fetchAsset,
+  AssetV1,
   update,
   fetchAssetsByCollection,
 } from "@metaplex-foundation/mpl-core";
 import axios from "axios";
 import { createSignerFromKeypair } from "@metaplex-foundation/umi";
-import { generateSigner, PublicKey, Umi } from "@metaplex-foundation/umi";
+import { generateSigner, Umi, PublicKey } from "@metaplex-foundation/umi";
 import { base58 } from "@metaplex-foundation/umi/serializers";
+import { PublicKey as SolanaPublicKey } from "@solana/web3.js";
 import { collectionAddress } from "@/lib/constants";
 
 export const uploadJsonData = async (data: Object) => {
@@ -62,7 +64,11 @@ export const mintNft = async (
           frozen: true,
         },
       ],
-    }).sendAndConfirm(umi);
+    }).sendAndConfirm(umi, {
+      confirm: {
+        commitment: "confirmed",
+      },
+    });
 
     console.log("mint tx:", base58.deserialize(res.signature)[0]);
 
@@ -84,12 +90,23 @@ export const updateNft = async (
   try {
     const collection = await fetchCollection(umi, collectionAddress);
     const asset = await fetchAsset(umi, assetAddress);
+    const collectionAuthoritySigner = createSignerFromKeypair(
+      umi,
+      umi.eddsa.createKeypairFromSecretKey(
+        base58.serialize(process.env.COLLECTION_AUTHORITY_SECRET_KEY!)
+      )
+    );
 
     const res = await update(umi, {
       asset: asset,
       collection: collection,
       uri: metadataUri,
-    }).sendAndConfirm(umi);
+      authority: collectionAuthoritySigner,
+    }).sendAndConfirm(umi, {
+      confirm: {
+        commitment: "confirmed",
+      },
+    });
 
     return {
       signature: base58.deserialize(res.signature)[0],
@@ -124,10 +141,23 @@ export const fetchNftsHandler = async (
   }
 };
 
+export const fetchNftsByOwner = async (umi: Umi, owner: SolanaPublicKey) => {
+  const assets = await fetchAssetsByCollection(umi, collectionAddress);
+  const ownerAssets: AssetV1[] = [];
+
+  assets.forEach((asset) => {
+    if (asset.owner.toString() == owner.toString()) {
+      ownerAssets.push(asset);
+    }
+  });
+
+  return ownerAssets;
+};
+
 export const createUser = async (
   umi: Umi,
   username: string,
-  vote: string,
+  proposal_index: string,
   vote_value: string,
   avatar: string
 ) => {
@@ -137,7 +167,7 @@ export const createUser = async (
       image: avatar,
       attributes: [
         {
-          [vote]: vote_value,
+          [proposal_index]: vote_value,
         },
       ],
       properties: {
@@ -164,5 +194,76 @@ export const createUser = async (
   } catch (err) {
     console.log(err);
     throw new Error("TX creation failed");
+  }
+};
+
+export const hasUserVotedOnProposal = async (
+  nfts: AssetV1[],
+  proposalIndex: string
+): Promise<boolean> => {
+  if (nfts.length === 0) return false;
+
+  const nft = nfts[0];
+  try {
+    const response = await axios.get(nft.uri);
+    const metadata = response.data;
+
+    if (metadata.attributes && Array.isArray(metadata.attributes)) {
+      return metadata.attributes.some((attr: any) =>
+        attr.hasOwnProperty(proposalIndex)
+      );
+    }
+  } catch (error) {
+    console.error("Error fetching NFT metadata:", error);
+  }
+
+  return false;
+};
+
+async function updateMetadata(
+  uri: string,
+  proposalIndex: string,
+  voteValue: string
+) {
+  const response = await axios.get(uri);
+  const metadata = response.data;
+
+  if (!metadata.attributes) {
+    metadata.attributes = [];
+  }
+
+  metadata.attributes.push({ [proposalIndex]: voteValue });
+
+  return metadata;
+}
+
+export const updateUser = async (
+  umi: Umi,
+  nft: AssetV1,
+  proposalIndex: string,
+  voteValue: string
+): Promise<string> => {
+  try {
+    const updatedMetadata = await updateMetadata(
+      nft.uri,
+      proposalIndex,
+      voteValue
+    );
+
+    const newMetadataUri = await uploadJsonData(updatedMetadata);
+
+    console.log("new metadata uri:", newMetadataUri);
+
+    const updateResult = await updateNft(
+      umi,
+      collectionAddress,
+      nft.publicKey as PublicKey,
+      newMetadataUri
+    );
+
+    return updateResult.signature;
+  } catch (error) {
+    console.error("Error updating user:", error);
+    throw new Error("Failed to update user");
   }
 };
