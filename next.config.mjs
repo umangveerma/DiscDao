b4e295b667f3997699802ac8fffa9942a9523c7f
vampo <umangboss786@gmail.com>
2024-09-11 14:03:07 +0530

    fix: CSP issues

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,13 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; connect-src 'self' https://api.dscvr.one https://api1.stg.dscvr.one https://*.helius-rpc.com https://*.ipfscdn.io;"
+            value: `
+              default-src 'self';
+              connect-src 'self' https://api.dscvr.one https://api1.stg.dscvr.one https://*.helius-rpc.com https://*.ipfscdn.io wss://*.helius-rpc.com;
+              script-src 'self' 'unsafe-eval' 'unsafe-inline';
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data: https:;
+            `.replace(/\s+/g, ' ').trim()
           },
         ],
       },
